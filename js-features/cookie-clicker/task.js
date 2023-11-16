85ea3d19191e6d24ded5d872c02cb1e7cc2fe775
ijwgt <66661966@bk.ru>
2023-11-16 16:12:34 +0300

    added task#2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,2 +1,27 @@
-let numOfClick = 0;
+const image = document.querySelector('.clicker__cookie');
+const counter = document.querySelector('#clicker__counter');
+const speed = document.querySelector('#clicker__speed');
+let time = Date.now();
+let previousClicks = 0;
+
+
+function change() {
+	image.width += 10;
+
+	let currentClicks = Number.parseInt(counter.textContent);
+	counter.textContent ++;
+
+	speed.textContent = (((Date.now() - time) / 1000) / (currentClicks - previousClicks)).toFixed(2);
+	previousClicks = currentClicks;
+}
+
+image.onclick = change;
+
+
+
+
+
+
+
+
 
