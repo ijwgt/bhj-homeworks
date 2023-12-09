a2f2ea7ae8c58ed946d1b6c3620054b4cf5c0c5d
ijwgt <66661966@bk.ru>
2023-12-09 16:08:33 +0300

    changed task#3

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -8,27 +8,28 @@ let lostCounter = 0;
 
 for (holeIndex = 1; holeIndex < 10; holeIndex++) {
 	let hole = getHole(holeIndex);
-    hole.addEventListener('click', function () {
-        if (hole.classList.contains('hole_has-mole')) {
-            deadCounter++;
-            dead.textContent = deadCounter;
-        } else {
-            lostCounter++;
-            lost.textContent = lostCounter;
-        }
-        if (deadCounter === 10) {
-            alert('Вы победили!');
-            deadCounter = 0;
-            lostCounter = 0;
-            dead.textContent = deadCounter;
-            lost.textContent = lostCounter;
-        }
-        if (lostCounter === 5) {
-            alert('Вы проиграли!');
-            deadCounter = 0;
-            lostCounter = 0;
-            dead.textContent = deadCounter;
-            lost.textContent = lostCounter;
+	hole.addEventListener('click', function () {
+		if (hole.classList.contains('hole_has-mole')) {
+			deadCounter++;
+			dead.textContent = deadCounter;
+		} else {
+			lostCounter++;
+			lost.textContent = lostCounter;
+			}
+
+		function reset (mess) {
+			alert(mess);
+			deadCounter = 0;
+			lostCounter = 0;
+			dead.textContent = deadCounter;
+			lost.textContent = lostCounter;
 		}
-    })
+
+		if (deadCounter === 10) {
+			reset('Вы победили!')
+		}
+		if (lostCounter === 5) {
+			reset('Вы проиграли!');
+		}
+	})
 }
