8be7d8a3d6d799a39c285903a59b8c5b1a8dd5e6
ijwgt <66661966@bk.ru>
2024-03-14 11:06:12 +0300

    fix task#2

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -6,8 +6,6 @@ xhr.onreadystatechange = function () {
 	if (xhr.readyState === xhr.DONE) {
 		const text = JSON.parse(xhr.responseText);
 		pollTitle.textContent = text.data.title;
-		const button = `<button class="poll__answer" onclick="alert('Спасибо, ваш голос засчитан!');">${pollAnswers}</button>`;
-		pollAnswers.insertAdjacentHTML('beforeend', button);
 
 		for (let answer of text.data.answers) {
 			const button = `<button class="poll__answer" onclick="alert('Спасибо, ваш голос засчитан!');">${answer}</button>`;
