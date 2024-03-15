3516ff161d02c6b72d888d714eeb3645d4614a54
ijwgt <66661966@bk.ru>
2024-03-15 14:39:05 +0300

    fixed todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,18 +1,17 @@
 const form = document.querySelector('.tasks__control');
 const tasksList = document.querySelector('.tasks__list');
+const tasksInput = document.querySelector('.tasks__input');
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
+	if (tasksInput.value.trim() !== '') {
+		createItems(form.elements.task__input.value);
 
-	createItems(form.elements.task__input.value);
-	
-	const removeTask = document.querySelector('.task__remove');
-	removeTask.addEventListener('click', () => {
-		event.target.parentElement.querySelector('.task').remove();
-	})
-
-	localStorage.setItem('task', tasksList.querySelector('.task__title').textContent);
-	console.log(localStorage.getItem('task'));
+		const removeTask = document.querySelector('.task__remove');
+		removeTask.addEventListener('click', () => {
+			event.target.parentElement.querySelector('.task').remove();
+		})
+	}
 
 	event.currentTarget.reset();
 })
@@ -23,3 +22,9 @@ function createItems(value) {
 	newTask.outerHTML = `<div class="task"><div class="task__title">${value}</div><a href="#" class="task__remove">&times;</a></div>`;
 }
 
+
+
+
+
+// localStorage.setItem('task', tasksList.querySelector('.task__title').textContent);
+// console.log(localStorage.getItem('task'));
